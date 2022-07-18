2b074cd94363053286954c4ee0e7eb26b979eaf7
simon waronja <simon.waronja@student.moringaschool.com>
2022-07-18 17:34:11 +0300

    commit tree 2bb5fa462a3053c5be873e854a4bc7ba491893ed

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+console.log (user.links.github);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name = "Thaiya" city = "Nairobi" color ="firebrick"/>
+      <About bio = {user.bio} links = {user.links}/>
+      <Links github = {user.links.github} linkedin = {user.links.linkedin}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Links = (props) => {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a><br />
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+  );
+};
+
+export default Links;
